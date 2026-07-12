7dbb3107c04f20a1070d823810da90f37bff8d3a
SantiaRey <sreyvaleiras@gmail.com>
2026-07-12 16:48:49 -0300

    creacion de obras html y js

---
+++ js/obras.js
@@ -0,0 +1,54 @@
+let nombres = [
+    "Pulse Room",
+    "Body Movies",
+    "Vectorial Elevation",
+    "33 Questions per Minute",
+    "Zoom Pavilion"
+];
+
+let anios = [
+    2006,
+    2001,
+    1999,
+    2000,
+    2015
+];
+
+let imagenes = [
+    "img/pulse-room.jpg",
+    "img/body-movies.jpg",
+    "img/vectorial-elevation.jpg",
+    "img/33-questions.jpg",
+    "img/zoom-pavilion.jpg"
+];
+
+let galeria = document.getElementById("galeria");
+
+for (let i = 0; i < nombres.length; i++) {
+
+    galeria.innerHTML += `
+
+        <article class="obra">
+
+            <h3>${nombres[i]}</h3>
+
+            <img src="${imagenes[i]}" alt="${nombres[i]}">
+
+            <p>Año: ${anios[i]}</p>
+
+        </article>
+
+    `;
+
+}
+
+let boton = document.getElementById("btnTamano");
+
+boton.addEventListener("click", cambiarDisenio);
+
+
+function cambiarDisenio() {
+
+    galeria.classList.toggle("galeriaGrande");
+
+}
